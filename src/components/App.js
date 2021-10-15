fe57587b2cf149719637b72fb090f136fb957d02
Bayheck <67043367+Bayheck@users.noreply.github.com>
2021-10-15 22:02:19 +0600

    fetched data from api

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,30 @@
+import React, {useState, useEffect} from 'react';
+// import "../styles/App.css";
+
 const App = () =>{
+
+    const [data, setData] = useState({});
+
+    useEffect(() =>{
+        fetch("https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations")
+        .then((res) => res.json())
+            .then((json) =>{
+                setData(json);
+            })
+    }, []);
+
     return(
-        <div>Hello World</div>
+        <div>
+            <div>
+                {(data && data.civilizations) ? 
+                    data.civilizations.map((civ) =>(
+                        <div>
+                            {civ.name}
+                        </div>
+                )) : " "}
+            </div>
+
+        </div>
     );
 }
 
